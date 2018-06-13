1cf0ffe7e5dc028903f32051a7c8ad55c1ee324a
Stepan Kondrat <stepan.kondrat@mail.ru>
2018-06-13 19:53:26 +0300

    Переписал функцию "points-counter" с использованием метода массивов "reduce"

--- js/data/points-counter.js
+++ js/data/points-counter.js
@@ -6,20 +6,22 @@ const pointsCounter = (array, livesLeft) => {
   if (array.length !== NUMBER_OF_GAMES) {
     return -1;
   }
-  let ourPoints = 0;
-  array.forEach((item) => {
+
+  let points = array.reduce((sumOfPoints, item) => {
     if (item.answer === true) {
-      ourPoints += 100;
+      sumOfPoints += 100;
     }
     if (item.time <= 10) {
-      ourPoints += 50;
+      sumOfPoints += 50;
     }
     if (item.time > 20) {
-      ourPoints -= 50;
+      sumOfPoints -= 50;
     }
+    return sumOfPoints;
 
-  });
-  return (BONUS_FOR_LIFE * livesLeft) + ourPoints;
+
+  }, 0);
+  return (BONUS_FOR_LIFE * livesLeft) + points;
 
 };
 
